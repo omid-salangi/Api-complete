a78be06d2e170376196185c099c1194d559d2b15
omid salangi <omidsalangi@outlook.com>
2022-02-06 12:32:57 +0000

    add webframe wrok and application layer

---
+++ WebFrameWork/Interface/IJwtServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Model;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Application.Interface
+{
+    public interface IJwtServices
+    {
+        Task<string> Generate(User user);
+        Task<IEnumerable<Claim>> _GetClaims(User user);
+    }
+}
