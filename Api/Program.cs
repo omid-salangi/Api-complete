1c1f4ec7586387de75500ab5b81b739a6df72675
omid salangi <omidsalangi@outlook.com>
2022-02-07 08:44:12 +0000

    add some

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,6 +1,40 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Data;
+using NLog;
+using NLog.Web;
+using Microsoft.EntityFrameworkCore;
+using Webframework.Configuration;
 
-app.MapGet("/", () => "Hello World!");
+var logger = NLog.LogManager.Setup().LoadConfigurationFromFile("/nlog.config").GetCurrentClassLogger();
+logger.Debug("init main");
 
-app.Run();
+try
+{
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // add mvc to project
+    builder.Services.AddControllersWithViews();
+    // add sql server 
+    builder.Services.AddDbContext<Context>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("BlogDbConnection"));
+    });
+
+    builder.Services.AddDependencyInjections();
+    var app = builder.Build();
+
+
+
+    app.Run();
+}
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
--- WebFrameWork/ViewModel/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Application.Model
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
